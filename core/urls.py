ee1494823c773042b15ca390e041741ad3a7fdf4
purnimapant77 <purnimaaaa7@gmail.com>
2026-07-18 21:12:02 +0545

    add all navbar pages

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/',views.about,name='about'),
+    path('about/', views.about, name='about'),
+    path('retreats/', views.retreats, name='retreats'),
+    path('trekking/', views.trekking, name='trekking'),
+    path('gallery/', views.gallery, name='gallery'),
+    path('reviews/', views.reviews, name='reviews'),
+    path('blogs/', views.blogs, name='blogs'),
+    path('contact/', views.contact, name='contact'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.shortcuts import render
-
 def home(request):
     return render(request, 'core/home.html')
 
 def about(request):
     return render(request, 'core/about.html')
+
+def retreats(request):
+    return render(request, 'core/retreats.html')
+
+def trekking(request):
+    return render(request, 'core/trekking.html')
+
+def gallery(request):
+    return render(request, 'core/gallery.html')
+
+def reviews(request):
+    return render(request, 'core/reviews.html')
+
+def blogs(request):
+    return render(request, 'core/blogs.html')
+
+def contact(request):
+    return render(request, 'core/contact.html')
